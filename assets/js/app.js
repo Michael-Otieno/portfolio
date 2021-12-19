8cb5193532a52faac7bb4380978aec940009d0c1
Michael-Otieno <michael.otieno@student.moringaschool.com>
2021-12-20 01:13:58 +0300

    add text animation

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,3 +27,29 @@ function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
+
+const text = document.querySelector('.fancy')
+const strText = text.textContent
+const splitText = strText.split("")
+text.textContent = "";
+for(let i=0; i<splitText.length; i++){
+  text.innerHTML += "<span>" + splitText[i] + "</span>"
+}
+let char = 0;
+let timer = setInterval(onTick, 50)
+
+function onTick(){
+  const span = text.querySelectorAll('span')[char];
+  span.classList.add('fade');
+  char++;
+
+  if(char==splitText.length){
+    complete();
+    return;
+  }
+}
+
+function complete(){
+  clearInterval(timer)
+  timer = null;
+}
