b0529154453f3043e40e62df47821f8a2a6c589f
Michael-Otieno <michael.otieno@student.moringaschool.com>
2022-02-08 18:27:28 +0300

    updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,54 @@
-const createNav = () =>{
-    let nav = document.querySelector('.navbar')
+const header = () =>{
+    let nav = document.querySelector('.primary-header')
 
     nav.innerHTML =`
     <div class="logo logox">
-        <p class="">m</p>
+      <p class="">m</p>
     </div>
-  
-    <div class="menu">
-        <ul class="menu-items">
-            <li>
-                <a href="index.html">about me</a>
-            </li>
-            <li>
-                <a href="projects.html">work</a>
-            </li>
-            <li>
-                <a href="contact.html">contact</a>
-            </li>
-        </ul>
-    </div>
+
+    <button aria-controls="primary-navigation" aria-expanded="false" class="mobile-nav-toggle">
+        <span class="sr-only"></span>
+     </button>
+    <nav>
+     <ul id="primary-navigation" data-visible="false" class="primary-navigation" style="display: flex;">
+         <li class="active">
+             <a href="index.html">
+                 <span aria-hidden="true" class='pe-2'>1</span>About
+             </a>
+         </li>
+         <li>
+             <a href="projects.html">
+                 <span aria-hidden="true" class='pe-2'>2</span>Work
+             </a>
+         </li>
+         <li>
+             <a href="contact.html">
+                 <span aria-hidden="true" class='pe-2'>3</span>contact
+             </a>
+         </li>
+         
+     </ul>
+    </nav>
     `
 }
-createNav()
+header()
+
+const primarynav= document.querySelector('.primary-navigation')
+const mobilenav= document.querySelector('.mobile-nav-toggle')
+
+mobilenav.addEventListener('click', ()=>{
+    const visible = primarynav.getAttribute('data-visible');
+
+    if(visible==='false'){
+        primarynav.setAttribute('data-visible',true);
+        mobilenav.setAttribute('arial-expanded',true)
+    }else if(visible==='true'){
+        primarynav.setAttribute('data-visible',false)
+        mobilenav.setAttribute('arial-expanded',false)
+
+    }
+});
+
 
 const footer = () =>{
     let footer = document.querySelector('footer')
@@ -30,13 +57,13 @@ const footer = () =>{
     <div>
     <p class="footer-links">
       <span class="footer-heading">lets connect:</span>
-      <a href="">
+      <a href="mailto:m.otieno205@gmail.com" target="_blank">
         <img src="/assets/images/gmail.png" alt="" width="20">
       </a>
-      <a href="">
+      <a href="https://www.linkedin.com/in/michael-otieno-924157217?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BEIfYgD4aSXG%2FPnZ%2B0YZuXA%3D%3D" target="_blank">
         <img src="/assets/images/linkedin.png" alt="" width="20">
       </a>
-      <a href="">
+      <a href="https://github.com/Michael-Otieno" target="_blank">
         <img src="/assets/images/github.png" alt="" width="20">
       </a>
       
